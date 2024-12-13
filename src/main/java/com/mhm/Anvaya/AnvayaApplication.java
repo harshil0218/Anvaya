342d3070f61ef505bfa316ffef879f3a1cf6d8a7
harshil0218 <malaviyah.218@gmail.com>
2024-12-13 19:05:46 +0530

    user auth project started

---
+++ src/main/java/com/mhm/Anvaya/AnvayaApplication.java
@@ -0,0 +1,13 @@
+package com.mhm.Anvaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnvayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnvayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhm/Anvaya/AnvayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhm.Anvaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnvayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
